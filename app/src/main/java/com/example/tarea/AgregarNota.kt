f85d72d2df533dadb36bd500649b7b4591aeb499
DarielNnexovery <darielulatelara@gmail.com>
2026-09-10 22:38:54 -0600

    Comit clase 3

--- app/src/main/java/com/example/tarea/AgregarNota.kt
+++ app/src/main/java/com/example/tarea/AgregarNota.kt
@@ -1,31 +1,20 @@
 package com.example.tarea
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.tarea.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding : ActivityMainBinding
-
+class AgregarNota : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_agregar_nota)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        binding.FABAgregarNota.setOnClickListener {
-            startActivity(Intent(applicationContext, AgregarNota::class.java ))
         }
     }
 }
